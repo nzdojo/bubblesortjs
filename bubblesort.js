3c7e66847066c93910865d5ab1759ab2d1b8a510
Teamzone <teamzone@aboutagile.com>
2017-08-10 00:49:11 +0000

    Files for bubbesort, Initial Commit

---
+++ bubblesort.js
@@ -0,0 +1,15 @@
+function bubbleSort(items)
+{
+    var swapped;
+    do {
+        swapped = false;
+        for (var i=0; i < items.length-1; i++) {
+            if (items[i] > items[i+1]) {
+                var temp = items[i];
+                items[i] = items[i+1];
+                items[i+1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
